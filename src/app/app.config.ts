cd584c707c17e03a9f4f6c6c8deebe1fdf36662a
Romina Martínez <romina.martinez496@gmail.com>
2025-09-21 09:25:13 -0300

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,6 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient() // <-- Y lo agrego acá. ¡Listo!
+    provideHttpClient() // <-- Y lo agrego acá, listo!
   ]
 };
